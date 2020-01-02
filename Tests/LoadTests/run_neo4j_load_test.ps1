0d0e4c18a7ecff745f1adc094f5e6d95e3425f60
NickGraham101 <nick.graham101@googlemail.com>
2020-01-02 11:58:12 +0000

    Fixed casing and spacing

--- Tests/LoadTests/run_neo4j_load_test.ps1
+++ Tests/LoadTests/run_neo4j_load_test.ps1
@@ -1,185 +1,182 @@
-﻿param
+﻿[CmdletBinding()]
+param
 (
-    [string]$protocol="http",
-    [string]$serverName="localhost",
-    [int]$duration=120,
-    [int]$numberOfThreads=1,
-    [int]$numberOfConnections=1,
-   [int]$iterations=1,
-   [string]$outputFolder="test1",
-   [int]$hitRate=0,
-   [string]$SingleLabel="N",
-   [string]$BaseDir="F:\Jmeter",
-   [string]$jmxName="NeoLoadTest.jmx",
-   [int]$Seed1=1,
-   [int]$Seed2=1,
-   [int]$BoltRetries=2,
-   [int]$BoltConnectionTimeout=60
+	[string]$Protocol = "http",
+	[string]$ServerName = "localhost",
+	[int]$Duration = 120,
+	[int]$NumberOfThreads = 1,
+	[int]$NumberOfConnections = 1,
+	[int]$Iterations = 1,
+	[string]$OutputFolder = "test1",
+	[int]$HitRate = 0,
+	[string]$SingleLabel = "N",
+	[string]$BaseDir = "F:\Jmeter",
+	[string]$JmxTestFileName = "NeoLoadTest.jmx",
+	[int]$Seed1 = 1,
+	[int]$Seed2 = 1,
+	[int]$BoltRetries = 2,
+	[int]$BoltConnectionTimeout = 60
 
 )
 
 
-Write-Output "Protocol:" + $protocol;
-Write-Output "ServerName:" + $serverName;
-Write-Output "Durations:" + $duration;
-Write-Output "NumberOfThreads:" + $numberOfThreads;
+Write-Output "Protocol:" + $Protocol;
+Write-Output "ServerName:" + $ServerName;
+Write-Output "Durations:" + $Duration;
+Write-Output "NumberOfThreads:" + $NumberOfThreads;
 Write-Output "iterations:" + $interations;
 Write-Output "Single Label:" + $SingleLabel;
-Write-Output "hitRate override:" + $hitRate;
-Write-Output "number of connections:" + $numberOfConnections;
+Write-Output "hitRate override:" + $HitRate;
+Write-Output "number of connections:" + $NumberOfConnections;
 
 #exit;
 function Out-FileUtf8NoBom {
 
-  [CmdletBinding()]
-  param(
-    [Parameter(Mandatory, Position=0)] [string] $LiteralPath,
-    [switch] $Append,
-    [switch] $NoClobber,
-    [AllowNull()] [int] $Width,
-    [Parameter(ValueFromPipeline)] $InputObject
-  )
+	[CmdletBinding()]
+	param(
+		[Parameter(Mandatory, Position = 0)] [string] $LiteralPath,
+		[switch] $Append,
+		[switch] $NoClobber,
+		[AllowNull()] [int] $Width,
+		[Parameter(ValueFromPipeline)] $InputObject
+	)
 
-  #requires -version 3
+	#requires -version 3
 
-  # Make sure that the .NET framework sees the same working dir. as PS
-  # and resolve the input path to a full path.
-  [System.IO.Directory]::SetCurrentDirectory($PWD) # Caveat: .NET Core doesn't support [Environment]::CurrentDirectory
-  $LiteralPath = [IO.Path]::GetFullPath($LiteralPath)
+	# Make sure that the .NET framework sees the same working dir. as PS
+	# and resolve the input path to a full path.
+	[System.IO.Directory]::SetCurrentDirectory($PWD) # Caveat: .NET Core doesn't support [Environment]::CurrentDirectory
+	$LiteralPath = [IO.Path]::GetFullPath($LiteralPath)
 
-  # If -NoClobber was specified, throw an exception if the target file already
-  # exists.
-  if ($NoClobber -and (Test-Path $LiteralPath)) {
-    Throw [IO.IOException] "The file '$LiteralPath' already exists."
-  }
+	# If -NoClobber was specified, throw an exception if the target file already
+	# exists.
+	if ($NoClobber -and (Test-Path $LiteralPath)) {
+		Throw [IO.IOException] "The file '$LiteralPath' already exists."
+	}
 
-  # Create a StreamWriter object.
-  # Note that we take advantage of the fact that the StreamWriter class by default:
-  # - uses UTF-8 encoding
-  # - without a BOM.
-  $sw = New-Object IO.StreamWriter $LiteralPath, $Append
+	# Create a StreamWriter object.
+	# Note that we take advantage of the fact that the StreamWriter class by default:
+	# - uses UTF-8 encoding
+	# - without a BOM.
+	$StreamWriter = New-Object IO.StreamWriter $LiteralPath, $Append
 
-  $htOutStringArgs = @{}
-  if ($Width) {
-    $htOutStringArgs += @{ Width = $Width }
-  }
+	$HtOutStringArgs = @{ }
+	if ($Width) {
+		$HtOutStringArgs += @{ Width = $Width }
+	}
 
-  # Note: By not using begin / process / end blocks, we're effectively running
-  #       in the end block, which means that all pipeline input has already
-  #       been collected in automatic variable $Input.
-  #       We must use this approach, because using | Out-String individually
-  #       in each iteration of a process block would format each input object
-  #       with an indvidual header.
-  try {
-    $Input | Out-String -Stream @htOutStringArgs | % { $sw.WriteLine($_) }
-  } finally {
-    $sw.Dispose()
-  }
+	# Note: By not using begin / process / end blocks, we're effectively running
+	#       in the end block, which means that all pipeline input has already
+	#       been collected in automatic variable $Input.
+	#       We must use this approach, because using | Out-String individually
+	#       in each iteration of a process block would format each input object
+	#       with an indvidual header.
+	try {
+		$Input | Out-String -Stream @HtOutStringArgs | % { $StreamWriter.WriteLine($_) }
+	}
+	finally {
+		$StreamWriter.Dispose()
+	}
 
 }
     
 
 
 #set run params
-#$protocol = "http"
-#$serverName = "dfc-dev-stax-shared-neo4j-aci.westeurope.azurecontainer.io"
-#$serverName = "localhost"
-#$duration = "300"
+#$Protocol = "http"
+#$ServerName = "dfc-dev-stax-shared-neo4j-aci.westeurope.azurecontainer.io"
+#$ServerName = "localhost"
+#$Duration = "300"
 
-$numberOfThreads = 50
+$NumberOfThreads = 50
 
-if ( $protocol -eq "http" )
-{
-    $numberOfThreads = 100
+if ( $Protocol -eq "http" ) {
+	$NumberOfThreads = 100
 }
 
 # set paths
-$jmeter_bin = $BaseDir + "\apache-jmeter-5.1.1\bin"
-$output_base = $BaseDir + "\output"
-$output_dir = $output_base + "\" + $outputFolder
-$scriptName = $BaseDir + "\Neo4j\" + $jmxName #NeoLoadTestPortableSingleDriverWithPool.jmx"
-$outFilename = $output_dir + "\neo_request_output.csv"
+$JMeterBinDir = $BaseDir + "\apache-jmeter-5.1.1\bin"
+$OutputBaseDir = $BaseDir + "\output"
+$OutputDir = $OutputBaseDir + "\" + $OutputFolder
+$ScriptFullName = $BaseDir + "\Neo4j\" + $JmxTestFileName #NeoLoadTestPortableSingleDriverWithPool.jmx"
+$OutFilename = $OutputDir + "\neo_request_output.csv"
 
-Write-Output "output_dir:" + $output_dir
-Write-Output "scriptName:" + $scriptName
+Write-Output "output_dir:" + $OutputDir
+Write-Output "scriptName:" + $ScriptFullName
 
 # set output filenames
-$overall_results_summary = "neo_results_full.csv"
-$overall_agg_summary = "neo_aggregation_full.csv"
+$OverallResultsSummary = "neo_results_full.csv"
+$OverallAggSummary = "neo_aggregation_full.csv"
 
 
 
 # intialise load params
-$previousTargetHitRate = $Seed1
-$currentTargetHitRate = $Seed2
+$PreviousTargetHitRate = $Seed1
+$CurrentTargetHitRate = $Seed2
 
-if ( $hitRate -gt 0 )
-{
-    Write-Output "Setting up for a single interation at hit rate of " + $hitRate
-    $currentTargetHitRate = $hitRate
-    $iterations = 1
+if ( $HitRate -gt 0 ) {
+	Write-Output "Setting up for a single interation at hit rate of " + $HitRate
+	$CurrentTargetHitRate = $HitRate
+	$Iterations = 1
 }
 
-$loop_count = 0
+$LoopCount = 0
 #$interations = 10
 # begin loop
-While ( $loop_count -lt $iterations )
-{
-    $loop_count += 1
+While ( $LoopCount -lt $Iterations ) {
+	$LoopCount += 1
 
-    New-Item -Path $output_base -Name $outputFolder -ItemType "directory"
-    #clear down results file
-    Remove-Item $outFilename
+	New-Item -Path $OutputBaseDir -Name $OutputFolder -ItemType "directory"
+	#clear down results file
+	Remove-Item $OutFilename
 
-    # run test
-    Write-Output "Running JMETER  Test: " + $scriptName + "  Server:" + serverName + " Protocol:" + $protocol + " TargerRate: " + $currentTargetHitRate
-    cmd.exe /c $jmeter_bin\jmeter -n -t $scriptName -J HostName=$serverName -J Protocol=$protocol -J TargetRate=$currentTargetHitRate -J NumberOfThreads=$numberOfThreads -J Duration=$duration -j $output_dir/logfile$loop_count.log -J SingleLabel=$SingleLabel -J ResultsFile=$outFilename -J NumberOfConnections=$numberOfConnections -J BoltRetries=$BoltRetries -J BoltConnectionTimeout=$BoltConnectionTimeout
+	# run test
+	Write-Output "Running JMETER  Test: " + $ScriptFullName + "  Server:" + serverName + " Protocol:" + $Protocol + " TargerRate: " + $CurrentTargetHitRate
+	cmd.exe /c $JMeterBinDir\jmeter -n -t $ScriptFullName -J HostName=$ServerName -J Protocol=$Protocol -J TargetRate=$CurrentTargetHitRate -J NumberOfThreads=$NumberOfThreads -J Duration=$Duration -j $OutputDir/logfile$LoopCount.log -J SingleLabel=$SingleLabel -J ResultsFile=$OutFilename -J NumberOfConnections=$NumberOfConnections -J BoltRetries=$BoltRetries -J BoltConnectionTimeout=$BoltConnectionTimeout
 
-    # generate aggregation report
-    cmd.exe /c $jmeter_bin\PluginsManagerCMD.bat --tool Reporter --generate-csv $output_dir\Aggregate_Report_$currentTargetHitRate.csv --input-jtl $outFilename --plugin-type AggregateReport
+	# generate aggregation report
+	cmd.exe /c $JMeterBinDir\PluginsManagerCMD.bat --tool Reporter --generate-csv $OutputDir\Aggregate_Report_$CurrentTargetHitRate.csv --input-jtl $OutFilename --plugin-type AggregateReport
 
-    # generate graphs??
-    cmd.exe /c  $jmeter_bin\PluginsManagerCMD.bat --tool Reporter --generate-png $output_dir\hits_per_second_$currentTargetHitRate.png --input-jtl $outFilename --plugin-type HitsPerSecond --width 800 --height 400 --granulation 10000
-    cmd.exe /c  $jmeter_bin\PluginsManagerCMD.bat --tool Reporter --generate-png $output_dir\ResponseTimesOverTime_$currentTargetHitRate.png --input-jtl $outFilename --plugin-type ResponseTimesOverTime --width 800 --height 400 --granulation 10000
-    cmd.exe /c  $jmeter_bin\PluginsManagerCMD.bat --tool Reporter --generate-png $output_dir\TransactionsPerSecond_$currentTargetHitRate.png --input-jtl $outFilename --plugin-type TransactionsPerSecond --width 800 --height 400 --granulation 10000
+	# generate graphs??
+	cmd.exe /c  $JMeterBinDir\PluginsManagerCMD.bat --tool Reporter --generate-png $OutputDir\hits_per_second_$CurrentTargetHitRate.png --input-jtl $OutFilename --plugin-type HitsPerSecond --width 800 --height 400 --granulation 10000
+	cmd.exe /c  $JMeterBinDir\PluginsManagerCMD.bat --tool Reporter --generate-png $OutputDir\ResponseTimesOverTime_$CurrentTargetHitRate.png --input-jtl $OutFilename --plugin-type ResponseTimesOverTime --width 800 --height 400 --granulation 10000
+	cmd.exe /c  $JMeterBinDir\PluginsManagerCMD.bat --tool Reporter --generate-png $OutputDir\TransactionsPerSecond_$CurrentTargetHitRate.png --input-jtl $OutFilename --plugin-type TransactionsPerSecond --width 800 --height 400 --granulation 10000
 
     
-    cmd.exe /c  $jmeter_bin\PluginsManagerCMD.bat --tool Reporter --generate-png $output_dir\hits_per_second_a_$currentTargetHitRate.png --input-jtl $outFilename --plugin-type HitsPerSecond --width 800 --height 600 --granulation 20000
-    cmd.exe /c  $jmeter_bin\PluginsManagerCMD.bat --tool Reporter --generate-png $output_dir\ResponseTimesOverTime_a_$currentTargetHitRate.png --input-jtl $outFilename --plugin-type ResponseTimesOverTime --width 600 --height 400 --granulation 20000
-    cmd.exe /c  $jmeter_bin\PluginsManagerCMD.bat --tool Reporter --generate-png $output_dir\TransactionsPerSecond_a_$currentTargetHitRate.png --input-jtl $outFilename --plugin-type TransactionsPerSecond --width 600 --height 400 --granulation 20000
-    #cmd.exe /v $jmeter_bin\PluginsManagerCMD.bat --tool Reporter --generate-png "F:\Jmeter\output\ResponseTimesOverTime_TEST.png" --input-jtl "F:\Jmeter\output\http_request_output.csv" --plugin-type ResponseTimesOverTime
-    # add to / initialise overarching results file
-    $output = get-content $outFilename 
-    if( $loop_count -eq 1 )
-    {
-        #initalise overarching file
-        $output | select -first 1 | foreach  { $_ } | Out-FileUtf8NoBom  $output_dir\$overall_results_summary 
-    }
-    $output | select -skip 1 | foreach  { $_ } | Out-FileUtf8NoBom -Append $output_dir\$overall_results_summary #>> $output_base\$overall_results_summary
+	cmd.exe /c  $JMeterBinDir\PluginsManagerCMD.bat --tool Reporter --generate-png $OutputDir\hits_per_second_a_$CurrentTargetHitRate.png --input-jtl $OutFilename --plugin-type HitsPerSecond --width 800 --height 600 --granulation 20000
+	cmd.exe /c  $JMeterBinDir\PluginsManagerCMD.bat --tool Reporter --generate-png $OutputDir\ResponseTimesOverTime_a_$CurrentTargetHitRate.png --input-jtl $OutFilename --plugin-type ResponseTimesOverTime --width 600 --height 400 --granulation 20000
+	cmd.exe /c  $JMeterBinDir\PluginsManagerCMD.bat --tool Reporter --generate-png $OutputDir\TransactionsPerSecond_a_$CurrentTargetHitRate.png --input-jtl $OutFilename --plugin-type TransactionsPerSecond --width 600 --height 400 --granulation 20000
+	#cmd.exe /v $JMeterBinDir\PluginsManagerCMD.bat --tool Reporter --generate-png "F:\Jmeter\output\ResponseTimesOverTime_TEST.png" --input-jtl "F:\Jmeter\output\http_request_output.csv" --plugin-type ResponseTimesOverTime
+	# add to / initialise overarching results file
+	$Output = get-content $OutFilename 
+	if ( $LoopCount -eq 1 ) {
+		#initalise overarching file
+		$Output | select -first 1 | foreach { $_ } | Out-FileUtf8NoBom  $OutputDir\$OverallResultsSummary 
+	}
+	$Output | select -skip 1 | foreach { $_ } | Out-FileUtf8NoBom -Append $OutputDir\$OverallResultsSummary #>> $OutputBaseDir\$OverallResultsSummary
 
-    #add to / initialise overaching aggregation report
-    $output2 = get-content $output_dir\Aggregate_Report_$currentTargetHitRate.csv
-    if( $loop_count -eq 1 )
-    {
-        #initalise overarching file
-        #$output2 | select -first 1 | foreach  {"protocol,target_workload," + $_ } > $output_base\$overall_agg_summary
-        Write-Output "Protocol,Target Workload,Request,# Samples,Average,Median,90% Line,95% Line,99% Line,Minimum,Maximum,Error %,Throughput,Bandwith,Stddev" | Out-FileUtf8NoBom  $output_dir\$overall_agg_summary
-    }
-    $output2 | select -skip 1 | foreach  { $protocol + "," + $currentTargetHitRate + "," + $_ } | Out-FileUtf8NoBom -Append $output_dir\$overall_agg_summary
+	#add to / initialise overaching aggregation report
+	$Output2 = get-content $OutputDir\Aggregate_Report_$CurrentTargetHitRate.csv
+	if ( $LoopCount -eq 1 ) {
+		#initalise overarching file
+		#$Output2 | select -first 1 | foreach  {"protocol,target_workload," + $_ } > $OutputBaseDir\$OverallAggSummary
+		Write-Output "Protocol,Target Workload,Request,# Samples,Average,Median,90% Line,95% Line,99% Line,Minimum,Maximum,Error %,Throughput,Bandwith,Stddev" | Out-FileUtf8NoBom  $OutputDir\$OverallAggSummary
+	}
+	$Output2 | select -skip 1 | foreach { $Protocol + "," + $CurrentTargetHitRate + "," + $_ } | Out-FileUtf8NoBom -Append $OutputDir\$OverallAggSummary
 
 
-    # add to overarching aggregration report
-    # https://stackoverflow.com/questions/9813228/add-rows-to-csv-file-in-powershell
+	# add to overarching aggregration report
+	# https://stackoverflow.com/questions/9813228/add-rows-to-csv-file-in-powershell
 
-    # increment load params
-    $newTargetHitRate = $previousTargetHitRate + $currentTargetHitRate
-    $previousTargetHitRate = $currentTargetHitRate
-    $currentTargetHitRate = $newTargetHitRate
+	# increment load params
+	$newTargetHitRate = $PreviousTargetHitRate + $CurrentTargetHitRate
+	$PreviousTargetHitRate = $CurrentTargetHitRate
+	$CurrentTargetHitRate = $newTargetHitRate
 
 }
 # end loop
 
 # generate overarching test report
-Remove-Item  -Recurse $output_dir\report
+Remove-Item  -Recurse $OutputDir\report
 
-cmd.exe /c $jmeter_bin\jmeter -g $output_dir\$overall_results_summary -o $output_dir\report
+cmd.exe /c $JMeterBinDir\jmeter -g $OutputDir\$OverallResultsSummary -o $OutputDir\report
