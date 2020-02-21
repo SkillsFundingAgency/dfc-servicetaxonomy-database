a25f3bb0508701a5c1d11145e49b7ae265c8c168
NickGraham101 <nick.graham101@googlemail.com>
2020-02-21 14:18:59 +0000

    Add cypher script to import shell script

--- Resources/Dockerfiles/Neo4jScripts/invoke-neo4j-import.sh
+++ Resources/Dockerfiles/Neo4jScripts/invoke-neo4j-import.sh
@@ -11,3 +11,9 @@ do
     echo "import failed, sleeping"
     sleep 10
 done
+
+until cat /scripts/neo4j-rename-esco-occupation-hidden-label-to-alt-label | /var/lib/neo4j/bin/cypher-shell --format plain -u neo4j -p $NEO4J_PWD 
+do
+    echo "import failed, sleeping"
+    sleep 10
+done
