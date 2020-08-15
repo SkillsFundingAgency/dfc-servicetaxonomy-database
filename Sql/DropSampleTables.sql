319015cd4c7c033bd1dbd5c5a50b6d573aa5e6fa
olusola-adio <olusola_adio@hotmail.com>
2020-08-15 08:37:24 +0100

    IF ELSE

--- Sql/DropSampleTables.sql
+++ Sql/DropSampleTables.sql
@@ -1,16 +1,20 @@
 /****** Object:  Table [dbo].[SampleTable1]    Script Date: 14/08/2020 23:37:44 ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SampleTable1]') AND type in (N'U'))
+BEGIN
     DROP TABLE [dbo].[SampleTable1];
     PRINT N'[dbo].[SampleTable1] DROPPED';
+END
 ELSE
+BEGIN
     PRINT N'[dbo].[SampleTable1] NOT FOUND';
-GO
+END
 
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SampleTable2]') AND type in (N'U'))
-    DROP TABLE [dbo].[SampleTable1];
+BEGIN
+    DROP TABLE [dbo].[SampleTable2];
     PRINT N'[dbo].[SampleTable2] DROPPED';
+END
 ELSE
+BEGIN
     PRINT N'[dbo].[SampleTable2] NOT FOUND';
-GO
-
-
+END
