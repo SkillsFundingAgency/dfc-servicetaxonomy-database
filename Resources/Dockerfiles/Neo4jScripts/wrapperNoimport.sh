d54b1b3d6112271cbfbec1a459aeee2389aa30e6
olusola-adio <olusola_adio@hotmail.com>
2020-12-08 15:25:10 +0000

    test

--- Resources/Dockerfiles/Neo4jScripts/wrapperNoimport.sh
+++ Resources/Dockerfiles/Neo4jScripts/wrapperNoimport.sh
@@ -2,19 +2,9 @@
 
 #https://stackoverflow.com/questions/48357238/how-can-i-run-cypher-scripts-on-startup-of-a-neo4j-docker-container
 
-# Log the info with the same format as NEO4J outputs
-log_info() {
-  # https://www.howtogeek.com/410442/how-to-display-the-date-and-time-in-the-linux-terminal-and-use-it-in-bash-scripts/
-  printf '%s %s\n' "$(date -u +"%Y-%m-%d %H:%M:%S:%3N%z") INFO  Wrapper: $1"
-  return
-}
 
 # turn on bash's job control
 set -m
-
-# wait for Neo4j
-log_info "Waiting until neo4j stats at :7474 ..."
-wget --quiet --tries=10 --waitretry=2 -O /dev/null http://localhost:7474
 
 # Start the primary process and put it in the background
 /docker-entrypoint.sh neo4j &
