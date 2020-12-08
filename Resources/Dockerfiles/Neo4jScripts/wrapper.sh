2e6927016abaf158389e833e38f9a9a2f935483e
Himanshu Trivedi <himanshu.trivedi@hotmail.com>
2020-12-08 11:31:00 +0000

    Enabled import

--- Resources/Dockerfiles/Neo4jScripts/wrapper.sh
+++ Resources/Dockerfiles/Neo4jScripts/wrapper.sh
@@ -9,7 +9,7 @@ set -m
 /docker-entrypoint.sh neo4j &
 
 # Start the helper process
-#/scripts/invoke-neo4j-import.sh
+/scripts/invoke-neo4j-import.sh
 
 # the my_helper_process might need to know how to wait on the
 # primary process to start before it does its work and returns
