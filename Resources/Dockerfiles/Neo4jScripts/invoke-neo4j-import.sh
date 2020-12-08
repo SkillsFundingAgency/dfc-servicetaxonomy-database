3708c1c3e6b45dfda73bcfd8d23c9d9948136d00
Himanshu Trivedi <himanshu.trivedi@hotmail.com>
2020-12-08 14:13:32 +0000

    Revert to original wrapper and changes ram and cpu config

--- Resources/Dockerfiles/Neo4jScripts/invoke-neo4j-import.sh
+++ Resources/Dockerfiles/Neo4jScripts/invoke-neo4j-import.sh
@@ -1,18 +1,18 @@
 sed -i "s/__ESCOFILE__/$ESCOFILE/g" /scripts/neo4j-import
 
-until cat /scripts/neo4j-db-config | /var/lib/neo4j/bin/cypher-shell --format plain  -u neo4j -p $NEO4J_PWD
+until cat /scripts/neo4j-db-config | /var/lib/neo4j/bin/cypher-shell --format plain  -u neo4j -p $NEO4J_PWD >> dbconfig.log
 do
     echo "db config failed, sleeping"
     sleep 10
 done
 
-until cat /scripts/neo4j-import | /var/lib/neo4j/bin/cypher-shell --format plain -u neo4j -p $NEO4J_PWD 
+until cat /scripts/neo4j-import | /var/lib/neo4j/bin/cypher-shell --format plain -u neo4j -p $NEO4J_PWD >> import.log
 do
     echo "import failed, sleeping"
     sleep 10
 done
 
-until cat /scripts/neo4j-rename-esco-occupation-hidden-label-to-alt-label | /var/lib/neo4j/bin/cypher-shell --format plain -u neo4j -p $NEO4J_PWD 
+until cat /scripts/neo4j-rename-esco-occupation-hidden-label-to-alt-label | /var/lib/neo4j/bin/cypher-shell --format plain -u neo4j -p $NEO4J_PWD esco.log
 do
     echo "import failed, sleeping"
     sleep 10
