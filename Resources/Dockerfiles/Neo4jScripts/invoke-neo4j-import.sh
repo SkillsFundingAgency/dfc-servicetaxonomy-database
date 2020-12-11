b33a6765d78ae50ad532e53416e99a8327bd4ced
Himanshu Trivedi <himanshu.trivedi@hotmail.com>
2020-12-11 17:31:55 +0000

    Final Commit before Merging to Dev

--- Resources/Dockerfiles/Neo4jScripts/invoke-neo4j-import.sh
+++ Resources/Dockerfiles/Neo4jScripts/invoke-neo4j-import.sh
@@ -1,18 +1,18 @@
 sed -i "s/__ESCOFILE__/$ESCOFILE/g" /scripts/neo4j-import
 
-until cat /scripts/neo4j-db-config | /var/lib/neo4j/bin/cypher-shell --format plain  -u neo4j -p $NEO4J_PWD >> dbconfig.log
+until cat /scripts/neo4j-db-config | /var/lib/neo4j/bin/cypher-shell --format plain  -u neo4j -p $NEO4J_PWD
 do
     echo "db config failed, sleeping"
     sleep 10
 done
 
-until cat /scripts/neo4j-import | /var/lib/neo4j/bin/cypher-shell --format plain -u neo4j -p $NEO4J_PWD >> import.log
+until cat /scripts/neo4j-import | /var/lib/neo4j/bin/cypher-shell --format plain -u neo4j -p $NEO4J_PWD
 do
     echo "import failed, sleeping"
     sleep 10
 done
 
-until cat /scripts/neo4j-rename-esco-occupation-hidden-label-to-alt-label | /var/lib/neo4j/bin/cypher-shell --format plain -u neo4j -p $NEO4J_PWD >> esco.log
+until cat /scripts/neo4j-rename-esco-occupation-hidden-label-to-alt-label | /var/lib/neo4j/bin/cypher-shell --format plain -u neo4j -p $NEO4J_PWD
 do
     echo "import failed, sleeping"
     sleep 10
